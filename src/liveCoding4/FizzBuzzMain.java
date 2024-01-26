d394ce0aae3d086a9976b576538ee6d74e47ab7a
PaulinaMB <paulinamendozabernal@gmail.com>
2024-01-26 14:23:14 -0600

    Archivos desarrollados para hacer el ejercico 4 de live coding

---
+++ src/liveCoding4/FizzBuzzMain.java
@@ -0,0 +1,10 @@
+package liveCoding4;
+/*Retroalimentación: La desarrolladora presento dificultades al inicio, pero supo como realizar el codigo después de un momento, tuvo su camara encendida y explico el código conforme lo fue haciendo.*/
+
+
+public class FizzBuzzMain{
+public static void main(String[] args) {
+	FizzBuzz fizzbuzz = new FizzBuzz();
+	fizzbuzz.imprimir();
+}
+}
